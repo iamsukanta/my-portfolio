dfea1f1ae16dcfeecf0f5811d116a95bd4079478
iamsukanta <sukantabalacste2016@gmail.com>
2025-04-07 23:31:44 +0200

    Email error updated with defined

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import styled from 'styled-components';
+import { email } from '@config';
 import { navDelay, loaderDelay } from '@utils';
 import { usePrefersReducedMotion } from '@hooks';
 
@@ -60,28 +61,20 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
+  const two = <h2 className="big-heading">Sukanta Bala.</h2>;
   const three = <h3 className="big-heading">I build things for the web.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
+        Full Stack Engineer with 7+ years of experience in building scalable and maintainable
+        applications, ensuring efficiency and reliability. Passionate about delivering scalable,
+        high-performance solutions in Agile/Scrum environments.
       </p>
     </>
   );
   const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
+    <a className="email-link" href={`mailto:${email}`}>
+      Say Hello
     </a>
   );
 
--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: 'sukantabalacste28@gmail.com',
 
   socialMedia: [
     {
